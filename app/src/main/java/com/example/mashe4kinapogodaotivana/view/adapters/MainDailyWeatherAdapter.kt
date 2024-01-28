4311b41cd3193e4f161239e57cf68f09926d09e1
ivanm <parfeyevets.ivan@bk.ru>
2024-01-28 19:58:22 +0300

    Almost finished application

--- app/src/main/java/com/example/mashe4kinapogodaotivana/view/adapters/MainDailyWeatherAdapter.kt
+++ app/src/main/java/com/example/mashe4kinapogodaotivana/view/adapters/MainDailyWeatherAdapter.kt
@@ -2,6 +2,7 @@ package com.example.mashe4kinapogodaotivana.view.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.mashe4kinapogodaotivana.R
 import com.example.mashe4kinapogodaotivana.businessModel.DailyWeatherModel
 import com.example.mashe4kinapogodaotivana.databinding.RvitemDailyMainBinding
@@ -27,6 +28,14 @@ class MainDailyWeatherAdapter : BaseAdapter<DailyWeatherModel>() {
     inner class DailyWeatherViewHolder(private val itemBinding: RvitemDailyMainBinding) :
         BaseViewHolder(itemBinding.root) {
         override fun bindView(position: Int) {
+            //Решение бага с повторяющимся выделением в rv
+            val itemData = mData[position]
+            val defaultTextColor = itemBinding.rvitemDailyDateMTV.textColors
+            if(position == 0){
+                itemBinding.rvitemDailyDateMTV.setTextColor(ContextCompat.getColor(itemBinding.rvitemDailyDateMTV.context,R.color.purple_200))
+            }
+            else{itemBinding.rvitemDailyDateMTV.setTextColor(defaultTextColor)}
+
             mData[position].apply {
                 itemBinding.rvitemDailyDateMTV.text = dt.toDateFormatOF(DAY_WEEK_NAME_LONG)
                 itemBinding.rvitemDailyWeatherIcIV.setImageResource(R.drawable.ic_menu_24)
